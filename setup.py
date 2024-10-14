c838eb61cf33c349439b74805a172ff28360bdc4
mgomeAstro <29342489+mgomezAstro@users.noreply.github.com>
2024-10-14 12:54:09 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
 from setuptools import setup
 
-description = """
-Omega2kpipe is an unofficial pipeline reduction of the near infrared camera Omega2000
-mounted in the 3.5m telescope at Calar Alto Observatory. The pipeline is based on
-the Omega2000 reduction techniques document that is available in its official website
-[here](https://www.caha.es/es/telescope-3-5m-2/omega-2000).
-"""
-
-install_requires = [
-    "astropy>=6.1.2",
-    "ccdproc>=2.4.2",
-]
+description = "Omega2kpipe is an unofficial pipeline reduction of the near infrared camera Omega2000 mounted in the 3.5m telescope at Calar Alto Observatory."
 
 setup(
     name="Omega2kpipe",
@@ -20,5 +10,4 @@ setup(
     author="mgomez",
     author_email="mgomez_astro@outlook.com",
     description=description,
-    install_requires=install_requires,
 )
